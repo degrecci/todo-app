bffd0ba1618cd8865efd4cc09ad5210814f0ddd2
degrecci <rickdegrecci@hotmail.com>
2017-04-16 19:33:08 -0300

    :sparkles: Add event

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -11,7 +11,8 @@ export default props => (
         </Grid>
 
         <Grid cols='12 3 2'>
-            <IconButton style='primary' icon='plus'></IconButton>
+            <IconButton style='primary' icon='plus'
+                onClick={props.handleAdd}></IconButton>
         </Grid>
     </div>
 )
