a809944bcdd78b263eb14b47a0fe08fe48519514
degrecci <rickdegrecci@hotmail.com>
2017-04-16 19:43:13 -0300

    :sparkles: onChange event

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -7,7 +7,9 @@ export default props => (
 
         <Grid cols='12 9 10'>
             <input id='description' className='form-control'
-                placeholder='Adicione uma tarefa'></input>
+                placeholder='Adicione uma tarefa'
+                onChange={props.handleChange}
+                value={props.description}></input>
         </Grid>
 
         <Grid cols='12 3 2'>
