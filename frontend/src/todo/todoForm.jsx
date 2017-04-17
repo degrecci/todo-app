adb998dcc2610c2b4a3329b6320a3e738f26ccd5
degrecci <henrique@mbp-de-bruno-2.upx.net.br>
2017-04-17 17:00:25 -0300

    :sparkles:  key events

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -2,13 +2,23 @@ import React from 'react'
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
 
-export default props => (
+export default props => {
+    const keyHandler = (e) => {
+        if(e.key === 'Enter') {
+            e.shiftKey ? props.handleSearch() : props.handleAdd()
+        } else if (e.key === 'Escape') {
+            props.handleClear()
+        }
+    }
+    
+    return (
     <div role='form' className='todoForm'>
 
         <Grid cols='12 9 10'>
             <input id='description' className='form-control'
                 placeholder='Adicione uma tarefa'
                 onChange={props.handleChange}
+                onKeyUp={keyHandler}
                 value={props.description}></input>
         </Grid>
 
@@ -21,4 +31,5 @@ export default props => (
                 onClick={props.handleClear}></IconButton>
         </Grid>
     </div>
-)
+    )
+}
